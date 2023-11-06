adea8f6e8ecadec5b164c9ffe9c9779141e59a2a
Eduardo Silva <eduardo.silva18@estudantes.ifms.edu.br>
2023-11-06 18:13:07 -0300

    media distancia

--- sketch_nov8a/sketch_nov8a.ino
+++ sketch_nov8a/sketch_nov8a.ino
@@ -1,3 +1,5 @@
+#define numeros_lidos 10
+
 const int led_vermelho = 3;
 const int led_amarelo = 2;
 const int led_verde = 4;
@@ -5,6 +7,9 @@ const int trig = 10;
 const int echo = A0;
 float distancia = 0;
 long tempo = 0;
+int index = 0;
+
+int leituras[numeros_lidos];
 
 void setup() {
   // put your setup code here, to run once:
@@ -30,12 +35,56 @@ void loop() {
     tempo = pulseIn(echo, HIGH);
     distancia = (tempo * 0.034) / 2;
 
-     //2.5: distancia marca copo e o sensor
-    float distancia_copovazio_senor = distancia - 2.5;
-  
-    if()
+    leituras[index] = distancia;
+
+    index = (index + 1) % numeros_lidos;
+
+    float soma = 0.0;
+    for(int i = 0; i < numeros_lidos; i++){
+        soma += leituras[i];
+    }
+
+    soma = soma / numeros_lidos;
+
+
+     //2.5: distancia marca copo 5e o sensor
+    float distancia_atual = soma - 4;
+    float copo_vazio;
+
+    if(distancia_atual >= 10 && distancia_atual <= 11.22){
+        delay(100);
+        digitalWrite(led_verde,LOW);
+        digitalWrite(led_amarelo,LOW);
+        digitalWrite(led_vermelho,LOW);
+        delay(50);
+        digitalWrite(led_verde,HIGH);
+        digitalWrite(led_amarelo,HIGH);
+        digitalWrite(led_vermelho,HIGH);
+        copo_vazio = distancia_atual;
+    }
+      //copo cheio
+      if(distancia_atual >= 4 && distancia_atual <= 5){
+        digitalWrite(led_verde,LOW);
+        digitalWrite(led_amarelo,LOW);
+        digitalWrite(led_vermelho,LOW);
+        digitalWrite(led_vermelho,HIGH);
+      }
+
+      
+      if(distancia_atual >= 3 && distancia_atual <= 3.5){
+        digitalWrite(led_verde,LOW);
+        digitalWrite(led_amarelo,LOW);
+        digitalWrite(led_vermelho,LOW);
+          digitalWrite(led_amarelo,HIGH);
+      }
     
 
-    Serial.println(distancia_copovazio_senor);
+    delay(100);
+    Serial.print("COPO VAZIO: ");
+    Serial.println(copo_vazio);
+    delay(100);
+    Serial.print("DISTANCIA ATUALA: ");
+    Serial.println(distancia_atual);
+
 
 }
