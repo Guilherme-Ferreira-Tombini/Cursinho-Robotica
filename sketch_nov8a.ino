2bca18882de131f0e77dfd0b8e7bd35b4a8200c6
Guilherme-Ferreira-Tombini <ge555449@gmail.com>
2023-11-08 16:56:50 -0300

    Adicionado variaveis de controle

--- sketch_nov8a.ino
+++ sketch_nov8a.ino
@@ -1,3 +1,9 @@
+const int led_vermelho = 3;
+const int led_amarelo = 2;
+const int led_verde = 4;
+const int trig = 10;
+const int echo = 0;
+
 void setup() {
   // put your setup code here, to run once:
 
